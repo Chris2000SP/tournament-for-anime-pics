536853ff53bd9ca8254ba42b9e51afc95753a7b1
Christopher Werner <chris2000sp@mailbox.org>
2025-12-05 15:57:51 +0100

    DSGVO Change

--- public/js/mail.js
+++ public/js/mail.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const user = "chris2000sp";
+  const user = "chris2000spsrv";
   const domain = "mailbox.org";
   const el = document.getElementById("mail");
   if (el) el.textContent = user + "@" + domain;
