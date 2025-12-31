935e6012f36341e83363b886b318f8cd7fbfd349
Christopher Werner <chris2000sp@googlemail.com>
2025-12-31 16:06:33 +0100

    public verzeichnis untracking erste

--- public/js/mail.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const user = "chris2000spsrv";
-  const domain = "mailbox.org";
-  const el = document.getElementById("mail");
-  if (el) el.textContent = user + "@" + domain;
-});
--- public/js/name.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const first_name = "Christopher"; // oder was auch immer~
-  const last_name = "Werner";
-
-  document.querySelectorAll(".name").forEach(el => {
-    el.textContent = first_name + " " + last_name;
-  });
-});
